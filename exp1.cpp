5623aeb3302830b94f1c3e32ab3b495868aa607c
feb213 <dineshdhumal.fe@gmail.com>
2025-11-04 22:24:28 +0530

    Update exp1.cpp

--- exp1.cpp
+++ exp1.cpp
@@ -1,44 +1,48 @@
-
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
 
-class BankAccount{
-  private:
-   string acc_name;
-   int acc_num;
-   string acc_type;
-   double bal;
-   
-   
-  public:
-    void details(string name,int acc_no,string type_of_acc,double balance){
-      acc_name=name;
-      acc_num=acc_no;
-      acc_type=type_of_acc;
-      bal=balance;
-      cout<<"your  initial balance:"<<bal<<endl; 
+class BankAccount {
+    string name;
+    int acc_no;
+    string acc_type;
+    float balance;
+
+public:
+    void assignValues(string n, int a, string type, float bal) {
+        name = n;
+        acc_no = a;
+        acc_type = type;
+        balance = bal;
     }
-    void deposite_amount(int amount){
-      bal+=amount;
-      
+
+    // Function to deposit amount
+    void deposit(float amount) {
+        balance += amount;
+        cout << "Amount deposited successfully.\n";
     }
-    
-    
-    void display(){
-      cout<<"Name:"<<acc_name<<endl;
-      cout<<"Account No:"<<acc_num<<endl;
-      cout<<"Type of account:"<<acc_type<<endl;
-      cout<<"your balance:"<<bal<<endl;
-   }
- };
- 
- int main(){
-   BankAccount obj;
-   obj.details("RAM",857856,"saving",210000);
-   obj.deposite_amount(2000);
-   obj.display();
-   return 0;
-   	
- }
- 
+    void withdraw(float amount) {
+        if (amount > balance)
+            cout << "Insufficient balance!\n";
+        else {
+            balance -= amount;
+            cout << "Amount withdrawn successfully.\n";
+        }
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+
+int main() {
+    BankAccount acc;
+
+    acc.assignValues("Dinesh", 12345, "Saving", 5000);
+
+    acc.deposit(1500);
+    acc.withdraw(2000);
+    acc.display();
+
+    return 0;
+}
