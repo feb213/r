f5291b699cc001895fc9292f3023cd6bd8e30ac9
feb213 <dineshdhumal.fe@gmail.com>
2025-11-05 07:05:30 +0530

    Update exp4.cpp

--- exp4.cpp
+++ exp4.cpp
@@ -1,43 +1,65 @@
 #include<iostream>
+#include<string>
 using namespace std;
+class student{
+    private:
+        string name;
+        int marks;
+    public:
+        void getData(){
+            cout<<"Enter name:";
+            cin>>name;
+            cout<<"Enter marks:";
+            cin>>marks;
+        }
+        void showData(){
+            cout<< name <<"\t"<<marks<<"\n";
+        }
 
-	class number
-	{
-	private:
-		int value;
-	public: number(int v=0)
-		{
-		
-		value=v;
-		
-		}
-		void display()
-		{
-		cout<<"\nvalue is :"<<value;
-		}
-		number operator++()//member function
-		{
-			value+=100;
-			return value;
-		}
-		friend number operator++(number&n,int);
-	};
-		number operator++(number&n,int)
-	{
-		number temp=n;
-		n.value += 100;
-	return temp;
-	}
-	
-	int main()
-	{
-	number num1(100),num2(300);
-	num1.display();
-	num2.display();
-	++num1;
-	num1.display();
-	num2++;
-	num2.display();
-	
-	return 0;
-	}
+        string getName(){return name;}
+        int getMarks(){return marks;}
+        //----------Overload Sort() fn-----------
+        static void sort(student s[],int n);
+        static void sort(student s[],int n,int);
+
+} ;
+void student::sort(student s[],int n){
+    for(int i=0; i < n-1 ;i++){
+        for(int j =i+1;j<n;j++){
+            if(s[i].getName()>s[j].getName()){
+                swap(s[i],s[j]);
+            }
+        }
+    } 
+    cout<<"\nSorted data by Name\n";
+    for(int i=0;i<n;i++)
+        s[i].showData();
+}
+void student::sort(student s[],int n,int){
+    for(int i=0; i < n-1 ;i++){
+        for(int j =i+1;j<n;j++){
+            if(s[i].getMarks()<s[j].getMarks()){
+                swap(s[i],s[j]);
+            }
+        }
+    } 
+    cout<<"\nSorted data by marks\n";
+    for(int i=0;i<n;i++)
+        s[i].showData();
+        
+}
+int main(){
+    int n;
+    cout<<"enter no of student :";
+    cin>>n;
+    student s[50];
+
+    for(int i =0 ;i<n;i++){
+        cout<<"\nEnter details of student "<<i+1<<":\n";
+        s[i].getData();
+    }
+    student::sort(s,n);
+    student::sort(s,n,1);
+
+    return 0;
+}
